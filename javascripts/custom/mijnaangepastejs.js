e51ee8b5dbf96fbd93692d4a669fdc5a94141bc8
Mevlut Simsek <Mevlutberzan0231@hotmail.com>
2025-01-02 12:49:54 +0100

    versie 7
    ik heb de volgende dingen aangepast.
    
    algemeen{
       1: foto's aangepast in de footer waar je nu ook een instagram icoontje kan zien op de laatste foto
    
    2: de juiste foto's toegevoegd en de namen van alle gerechten dat mewsim heeft. sommige hebben nog sutlac als foto dat komt, omdat ik geen foto heb van de mewsim gerecht zelf. Daarmee bedoel ik dat sommige gerechten bijv "Mewsim" ervoor hebben dus het is iets speciaals
    
    3: alle pagina hebben nu ook de jaartal
    script.
    
    4: de gerechten kun je nu hoveren.
    }
    
    index pagina{
       1: je ziet nu 5 gerechten per categorie en onderaan iedere categorie zie je een button die je verwijst naar de menu pagina
    
       2: datums aangepast van het laatste nieuws
    }
    
    menu_single{
       1: de stock, spicy en in voorraad labels uitgezet (ik heb gewoon commentaarregels ervoor gezet zelfde geldt voor de ratings)
    
    2: je kan in de titel nu de naam zien van het gerecht ook bij het "url" die je ziet op het midden van je scherm
    voorbeeld:      Home / Menu / Mozzarella Caprese
    
    3: tekst ook vertaald naar het nederlands die nog in engels stonden
    }
    
    menu{
    je kan nu alle mewsim gerechten zien met prijs en hover
    }
    
    het enige wat nu niet werkt is de contact page.

--- javascripts/custom/mijnaangepastejs.js
+++ javascripts/custom/mijnaangepastejs.js
@@ -6,53 +6,71 @@ document.getElementById('currentYear').textContent = new Date().getFullYear();
 // JSON-data voor ontbij/lunch
 const menuItems = [
   {
-    "name": "Menemen",
-    "image": "img/eten_fotos/menemen.jpg",
-    "description": "Een pittige Turkse roerei gemaakt met tomaten, paprika en specerijen.",
-    "price": "€ 7,50"
+    "name": "Mercimek Corbasi",
+    "image": "img/eten_fotos/mercimek_corbasi.jpg",
+        "price": "€ 6,75",
+    "description": "Linzensoep, een traditioneel Turks gerecht."
   },
   {
-    "name": "Sucuklu Yumurta",
-    "image": "img/eten_fotos/sucuklu_yumurta.jpg",
-    "description": "Gebakken eieren met Turkse sucuk (vleesworst) en tomaten.",
-    "price": "€ 6,75"
+    "name": "Mewsim Corbasi",
+    "image": "img/eten_fotos/sutlac.jpg",
+        "price": "€ 6,75",
+    "description": "Speciale mewsim soep."
   },
   {
-    "name": "Sigara Böreği",
-    "image": "img/eten_fotos/sigara_boregi.jpg",
-    "description": "Fritters gevuld met kaas en kruiden, vaak geserveerd als ontbijt.",
-    "price": "€ 9,50"
+    "name": "Mewsim Salata",
+    "image": "img/eten_fotos/sutlac.jpg",
+        "price": "€ 9,50",
+    "description": "Seizoenssalade met sla, komkommer, mais, wortel en dressing."
   },
   {
-    "name": "Kahvaltı Tabağı",
-    "image": "img/eten_fotos/kahvalti_tabagi.jpg",
-    "description": "Een traditioneel Turks ontbijt met verschillende kazen, olijven, brood, tomaten, komkommer en worst.",
-    "price": "€ 11,50"
+    "name": "Yoghurt Bowl",
+    "image": "img/eten_fotos/yoghurt_bowl.jpeg",
+        "price": "€ 12,50",
+    "description": "Een frisse en romige kom met yoghurt, perfect voor een gezond ontbijt of snack."
   },
   {
-    "name": "Kuymak",
-    "image": "img/eten_fotos/kuymak.jpg",
-    "description": "Een dunne, knapperige pizza met gehakt, tomaat, ui en kruiden.",
-    "price": "€ 12,50"
+    "name": "Mozzarella Caprese",
+    "image": "img/eten_fotos/mozzarella_caprese.jpg",
+        "price": "€ 12,50",
+    "description": "Een klassieke Italiaanse salade met romige mozzarella, tomaat en verse basilicum."
+  },
+  /*{
+    "name": "Somon Füme",
+    "image": "img/eten_fotos/somon_fume.jpg",
+        "price": "€ 14,90",
+    "description": "Rookzalige gerookte zalm, ideaal voor een lichte en smaakvolle maaltijd."
   },
   {
-    "name": "Peynirli Pide",
-    "image": "img/eten_fotos/pide_with_cheese.jpg",
-    "description": "Een brood gevuld met Turkse kaas en kruiden, vaak gegeten bij het ontbijt.",
-    "price": "€ 11,50"
+    "name": "Akdeniz Salatasi",
+    "image": "img/eten_fotos/akdeniz_salatasi.jpg",
+        "price": "€ 11,50",
+    "description": "Een frisse salade geïnspireerd door de smaken van de Middellandse Zee, met olijven, feta en groenten."
   },
   {
-    "name": "Bal-Kaymak",
-    "image": "img/eten_fotos/bal_Kaymak.jpg",
-    "description": "Vers brood met honing en room.",
-    "price": "€ 4,50"
+    "name": "Tavuk Crouton",
+    "image": "img/eten_fotos/tavuk_crouton.jpg",
+        "price": "€ 15,90",
+    "description": "Knapperige stukjes kip met croutons, ideaal als een hartige en vullende salade."
   },
   {
-    "name": "Kumpir",
-    "image": "img/eten_fotos/kumpir.jpg",
-    "description": "Een grote aardappel gevuld met boter, kaas, groenten en ham.",
-    "price": "€ 7,50"
-  }
+    "name": "Cevizli Kasik Salatasi",
+    "image": "img/eten_fotos/cevizli_kasik_salatasi.jpg",
+        "price": "€ 13,90",
+    "description": "Een salade met walnoten en een mix van frisse groenten, heerlijk als bijgerecht. "
+  },
+  {
+    "name": "Avokado Salatasi",
+    "image": "img/eten_fotos/avokado_salatasi.jpg",
+        "price": "€ 14,90",
+    "description": "Een gezonde salade met avocado en een lichte dressing, vol smaak en goed voor je gezondheid."
+  },
+  {
+    "name": "Karides Salatasi",
+    "image": "img/eten_fotos/karides_salatasi.jpg",
+        "price": "€ 13,90",
+    "description": "Een frisse salade met garnalen, perfect voor liefhebbers van zeevruchten"
+  }*/
 ];
 
 const menuContainer = document.getElementById('menu-items');
@@ -62,23 +80,19 @@ menuItems.forEach(item => {
   menuItemElement.classList.add('menu-item-list', 'col-md-6', 'col-sm-12');
   
   menuItemElement.innerHTML = `
+  <a href="menu_single.html?item=${encodeURIComponent(item.name)}" class="menu-item-link">
     <div class="item-img">
       <div class="overlay_content">
         <div class="overlay_item"> 
           <img src="${item.image}" alt="">
-          <div class="overlay">
-            <div class="icons">
-              <div class="button">
-              </div>
-              <a class="close-overlay hidden">x</a>
-            </div>
-          </div>
         </div>
       </div>
     </div>
-    <h3><a href="menu_single.html?item=${encodeURIComponent(item.name)}">${item.name}</a> <span class="price">${item.price}</span></h3>
+    <h3>${item.name}<span class="price">${item.price}</span></h3>
     <p>${item.description}</p>
-  `;
+  </a> 
+`;
+
 
   menuContainer.appendChild(menuItemElement);
 });
@@ -94,53 +108,83 @@ function openNewPage(itemName) {
 // JSON-data voor dinner
 const menuItemsa = [
   {
-"name": "Adana Kebab",
-"image": "img/eten_fotos/adana_kebab.jpg",
-"price": "€ 15,00",
-"description": "Gegrild lamsvlees kebab met specerijen en pitabrood."
-},
-{
-"name": "Iskender Kebab",
-"image": "img/eten_fotos/iskender_kebab.jpg",
-"price": "€ 18,00",
-"description": "Gebroken kebab geserveerd op pita met tomatensaus en yoghurt."
-},
-{
-"name": "Tavuk Shish",
-"image": "img/eten_fotos/tavuk_shish.jpg",
-"price": "€ 20,00",
-"description": "Gegrilde kipspies met groenten."
-},
-{
-"name": "Kuzu Tandir",
-"image": "img/eten_fotos/kuzu_tandir.jpg",
-"price": "€ 22,00",
-"description": "Langzaam gegaard lamsschenkel met kruiden en groenten."
-},
-{
-"name": "Lahmacun",
-"image": "img/eten_fotos/lahmacun.jpg",
-"price": "€ 14,00",
-"description": "Een Turkse pizza met gehakt en groenten."
-},
-{
-"name": "Köfte",
-"image": "img/eten_fotos/kofte.jpg",
-"price": "€ 17,00",
-"description": "Gehaktballetjes gekruid met kruiden en geserveerd met salade."
-},
-{
-"name": "Pide",
-"image": "img/eten_fotos/Pide.jpg",
-"price": "€ 19,00",
-"description": "Een Turks brood met diverse toppings zoals kaas, ei en kruiden."
-},
-{
-"name": "Dolma",
-"image": "img/eten_fotos/dolma.jpg",
-"price": "€ 21,00",
-"description": "Gevulde druivenbladeren met rijst, kruiden en kruiden."
-}
+    "name": "Adana Kebab",
+    "image": "img/eten_fotos/adana_kebab.jpg",
+        "price": "€ 22,90",
+    "description": "Pittige gehaktspies van de grill."
+  },
+  {
+    "name": "Sebzeli Kebab",
+    "image": "img/eten_fotos/sebzeli_kebab.jpg",
+        "price": "€ 23,90",
+    "description": "Een kebab met gegrilde groenten, een heerlijke combinatie van smaken."
+  },
+  {
+    "name": "Beyti Kebab",
+    "image": "img/eten_fotos/beyti_kebab.jpg",
+        "price": "€ 26,90",
+    "description": "Gegrild vlees in een zachte wrap, geserveerd met een heerlijke yoghurtsaus."
+  },
+  {
+    "name": "Shoarma",
+    "image": "img/eten_fotos/shoarma.jpg",
+        "price": "€ 21,90",
+    "description": "Mals vlees gekruid met aromatische specerijen, geserveerd met een frisse salade."
+  },
+  {
+    "name": "Sis Kebab",
+    "image": "img/eten_fotos/sis_kebab.jpg",
+        "price": "€ 27,90",
+    "description": "Traditionele kebab van gegrild vlees op een spies, een smaakvolle traktatie."
+  },/*
+  {
+    "name": "mewsim Köfte",
+    "image": "img/eten_fotos/sutlac.jpg",
+        "price": "€ 18,90",
+    "description": "Gehaktballetjes op de grill met patat."
+  },
+  {
+    "name": "Tavuk Sis",
+    "image": "img/eten_fotos/tavuk_sis.jpg",
+        "price": "€ 20,90",
+    "description": "Mals kipfilet aan een spies, gegrild tot perfectie."
+  },
+  {
+    "name": "Tavuk Göğsü",
+    "image": "img/eten_fotos/tavuk_gogsu.jpg",
+        "price": "€ 20,90",
+    "description": "Gegrilde kipfilet, een lichte en smakelijke keuze."
+  },
+  {
+    "name": "Kuzu Pirzola",
+    "image": "img/eten_fotos/kuzu_pirzola.jpg",
+        "price": "€ 28,90",
+    "description": "Gegrilde lamskoteletjes, vol van smaak en perfect voor een stevige maaltijd."
+  },
+  {
+    "name": "Ali Nazik",
+    "image": "img/eten_fotos/ali_nazik.jpg",
+        "price": "€ 27,90",
+    "description": "Gegrild vlees geserveerd op een bedje van romige yoghurt en aubergine."
+  },
+  {
+    "name": "Mix Grill",
+    "image": "img/eten_fotos/mix_grill.jpg",
+        "price": "€ 28,90",
+    "description": "Een variatie van gegrild vlees, perfect voor wie van verschillende smaken houdt."
+  },
+  {
+    "name": "Mix Grill (2 personen)",
+    "image": "img/eten_fotos/mix_grill.jpg",
+        "price": "€ 52,50",
+    "description": "Een royale portie mix grill, ideaal voor twee personen."
+  },
+  {
+    "name": "Mix Grill (3 personen)",
+    "image": "img/eten_fotos/mix_grill.jpg",
+        "price": "€ 75,00",
+    "description": "Een grote mix grill voor drie personen, voor een feestelijke maaltijd."
+  }*/
 ];
 
   const menuContainera = document.getElementById('menu-items2');
@@ -150,25 +194,18 @@ const menuItemsa = [
     menuItemElement.classList.add('menu-item-list', 'col-md-6', 'col-sm-12');
     
     menuItemElement.innerHTML = `
+    <a href="menu_single.html?item=${encodeURIComponent(item.name)}" class="menu-item-link">
       <div class="item-img">
         <div class="overlay_content">
           <div class="overlay_item"> 
             <img src="${item.image}" alt="">
-            <div class="overlay">
-              <div class="icons">
-                <div class="button">
-                  <a href="${item.image}" rel="lightbox"><i class="fa fa-expand"></i></a>
-                  <a href="menu_single.html?item=${encodeURIComponent(item.name)}"><i class="fa fa-shopping-cart"></i></a>
-                </div>
-                <a class="close-overlay hidden">x</a>
-              </div>
-            </div>
           </div>
         </div>
       </div>
-      <h3><a href="menu_single.html?item=${encodeURIComponent(item.name)}">${item.name}</a> <span class="price">${item.price}</span></h3>
+      <h3>${item.name}<span class="price">${item.price}</span></h3>
       <p>${item.description}</p>
-    `;
+    </a> 
+  `;
 
     menuContainera.appendChild(menuItemElement);
   });
@@ -183,44 +220,79 @@ const menuItemsa = [
 // JSON-data voor toetje
 const menuItemsc = [
   {
-    "name": "Baklava",
-    "image": "img/eten_fotos/baklava.jpg",
-    "description": "Een bladerdeeg met gehakte noten en siroop.",
-          "price": "€ 6,75"
-  
-  },
-  {
-    "name": "Revani",
-    "image": "img/eten_fotos/revani.jpg",
-    "description": "Een zoete taart gemaakt van griesmeel, gegarneerd met siroop.",
-          "price": "€ 6,75"
-  },
-  {
-    "name": "Künefe",
-    "image": "img/eten_fotos/kunefe.jpg",
-    "description": "Een Turkse dessert gemaakt van kaas en baklava-deeg.",
-          "price": "€ 6,75"
-  },
-  {
-    "name": "Sütlaç",
+    "name": "Sütlac",
     "image": "img/eten_fotos/sutlac.jpg",
     "description": "Een zoete rijstepap met kaneel en suiker.",
-          "price": "€ 6,75"
-  },
-  {
-    "name": "cheesecake",
-    "image": "img/eten_fotos/cheesecake.jpg",
-    "description": "Cheesecake gemaakt met kaneel",
-          "price": "€ 6,75"
+    "price": "€ 6,00"
   },
   {
     "name": "Kazandibi",
     "image": "img/eten_fotos/kazandibi.jpg",
     "description": "Een gebakken melkpudding.",
-          "price": "€ 6,75"
+    "price": "€ 6,50"
   },
-
+  {
+    "name": "Künefe",
+    "image": "img/eten_fotos/kunefe.jpg",
+    "description": "Een Turks dessert gemaakt van kaas en baklava-deeg.",
+    "price": "€ 8,90"
+  },
+  {
+    "name": "San Sebastian Cheesecake",
+    "image": "img/eten_fotos/san_sebastian_cheesecake.jpg",
+    "description": "Cheesecake gemaakt met een zachte, romige binnenkant.",
+    "price": "€ 8,50"
+  },
+  {
+    "name": "Cheesecake",
+    "image": "img/eten_fotos/cheesecake.jpg",
+    "description": "Klassieke cheesecake met een knapperige bodem.",
+    "price": "€ 8,50"
+  },/*
+  {
+    "name": "Red Velvet Cake",
+    "image": "img/eten_fotos/red_velvet_cake.jpg",
+    "description": "Een luchtige taart met een vleugje cacao.",
+    "price": "€ 8,50"
+  },
+  {
+    "name": "Cikolatali Sufle",
+    "image": "img/eten_fotos/cikolatali_sufle.jpg",
+    "description": "Chocolade lava cake met een warme kern.",
+    "price": "€ 8,90"
+  },
+  {
+    "name": "Baklava Burger",
+    "image": "img/eten_fotos/baklava_burger.jpeg",
+    "description": "Unieke combinatie van baklava in burgerstijl.",
+    "price": "€ 10,90"
+  },
+  {
+    "name": "Baklava Burger met IJs",
+    "image": "img/eten_fotos/baklava_burger_met_ijs.jpg",
+    "description": "Baklava burger geserveerd met vanille-ijs.",
+    "price": "€ 12,90"
+  },
+  {
+    "name": "Fruitschaal",
+    "image": "img/eten_fotos/fruitschaal.jpg",
+    "description": "Een frisse selectie van seizoensfruit.",
+    "price": "€ 7,50"
+  },
+  {
+    "name": "Diverse Soorten IJs (3 bollen)",
+    "image": "img/eten_fotos/diverse_soorten_ijs.jpg",
+    "description": "Keuze uit drie smaken ijs.",
+    "price": "€ 8,25"
+  },
+  {
+    "name": "Bolletje Vanille IJs",
+    "image": "img/eten_fotos/bolletje_vanille_ijs.jpg",
+    "description": "Een klassiek bolletje vanille-ijs.",
+    "price": "€ 3,00"
+  }*/
 ];
+
 
   const menuContainerc = document.getElementById('menu-items3');
 
@@ -228,27 +300,19 @@ const menuItemsc = [
     const menuItemElement = document.createElement('article');
     menuItemElement.classList.add('menu-item-list', 'col-md-6', 'col-sm-12');
     
-    menuItemElement.innerHTML = `
-      <div class="item-img">
-        <div class="overlay_content">
-          <div class="overlay_item"> 
-            <img src="${item.image}" alt="">
-            <div class="overlay">
-              <div class="icons">
-                <div class="button">
-                  <a href="${item.image}" rel="lightbox"><i class="fa fa-expand"></i></a>
-                  <a href="menu_single.html?item=${encodeURIComponent(item.name)}"><i class="fa fa-shopping-cart"></i></a>
-                </div>
-                <a class="close-overlay hidden">x</a>
-              </div>
-            </div>
-          </div>
+  menuItemElement.innerHTML = `
+  <a href="menu_single.html?item=${encodeURIComponent(item.name)}" class="menu-item-link">
+    <div class="item-img">
+      <div class="overlay_content">
+        <div class="overlay_item"> 
+          <img src="${item.image}" alt="">
         </div>
       </div>
-      <h3><a href="menu_single.html?item=${encodeURIComponent(item.name)}">${item.name}</a> <span class="price">${item.price}</span></h3>
-      <p>${item.description}</p>
-    `;
-
+    </div>
+    <h3>${item.name}<span class="price">${item.price}</span></h3>
+    <p>${item.description}</p>
+  </a> 
+`;
     menuContainerc.appendChild(menuItemElement);
   });
 
